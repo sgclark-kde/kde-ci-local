d4d6e889878930781b9ea002d01c8ebaac59dcac
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2015-10-21 14:21:47 -0700

    debug

--- helper/Platform.groovy
+++ helper/Platform.groovy
@@ -55,7 +55,7 @@ class Platform {
 			this.platformsToBuild << key
 			this.platformCompilers << compiler				
 		}
-		this.PlatformToCompiler << ["${key}" : compiler]
+		this.PlatformToCompiler << ["${key}" : ' ' + compiler]
 	}
 	def newTrack()	{		
 		platformCompilers = []		
