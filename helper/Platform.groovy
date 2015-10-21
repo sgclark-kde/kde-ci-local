7a2dbc48d88af484935bd08a71a511ad7ce79a2e
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2015-10-21 14:28:54 -0700

    debug

--- helper/Platform.groovy
+++ helper/Platform.groovy
@@ -76,7 +76,7 @@ class Platform {
 				} else {
 					this.shell = 'Shell'
 				}
-				def compiler = PlatformToCompiler.getAt(platform)
+				def compiler = PlatformToCompiler.find { key, value -> key == platform }
 				
 				project / builders <<
 				'org.jenkinsci.plugins.conditionalbuildstep.singlestep.SingleConditionalBuilder' {
