769be5a0a1273542216b1314626f2cd262aa83f8
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2015-10-20 20:08:35 -0700

    Change linux to ubuntu to match docker slave name.
    Misc documentation. Rename script to run on docker host.
    Update metadata.

--- docker/run-data-volumes-on-the-slave-host.sh
+++
@@ -1,3 +0,0 @@
-echo "Starting data volumes"
-docker run --name HOME sgclarkkde/kde-slave-home
-docker run --name INSTALL sgclarkkde/kde-slave-install
---
+++ docker/run-once-on-the-slave-host-after-prepare-slaves.sh
@@ -0,0 +1,5 @@
+echo "Starting data volumes"
+docker run --name HOME sgclarkkde/kde-slave-home
+docker run --name INSTALL sgclarkkde/kde-slave-install
+echo "Starting ubuntu slave"
+docker run --name ubuntu --volumes-from=HOME --volumes-from=INSTALL sgclarkkde/kde-build-ubuntu
