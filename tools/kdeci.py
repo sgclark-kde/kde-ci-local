69ff31b134bec30b94b1fd95da23135867736c5a
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2015-10-21 18:17:54 -0700

    regex

--- tools/kdeci.py
+++ tools/kdeci.py
@@ -84,7 +84,7 @@ def check_jenkins_environment():
     # Do we have a job name?
     if 'JOB_NAME' in os.environ:
         # Split it out
-        jobMatch = re.match("(?P<project>[^\s]+)\s?(?P<branch>[^\s]+)\s?(?P<branchGroup>[^\s]+)[/PLATFORM=]?(?P<platform>[^/]+)[,compiler=]?(?P<compiler>[^=]+)?", os.environ['JOB_NAME'])
+        jobMatch = re.match("(?P<project>[^\s]+)\s?(?P<branch>[^\s]+)\s?(?P<branchGroup>[^/]+)[PLATFORM=]?(?P<platform>[^,]+)[compiler=]?(?P<compiler>[^_/+)?", os.environ['JOB_NAME'])
         # Now transfer in any non-None attributes
         # If we have the project name, transfer it
         if jobMatch.group('project') is not None:
