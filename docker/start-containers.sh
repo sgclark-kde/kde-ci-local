2acf0fe25745005859a5ef10ee6ae666513c70f7
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2015-11-13 14:53:56 -0800

    Docker adjustments for production

--- docker/start-containers.sh
+++ docker/start-containers.sh
@@ -2,4 +2,4 @@ echo "Starting data volumes"
 docker run --name HOME sgclarkkde/kde-slave-home
 docker run --name INSTALL sgclarkkde/kde-slave-install
 echo "Starting ubuntu slave"
-docker run --name ubuntu --volumes-from=HOME --volumes-from=INSTALL sgclarkkde/kde-build-ubuntu
+docker run --name ubuntu --volumes-from=HOME --volumes-from=INSTALL sgclarkkde/kde-slave-ubuntu
