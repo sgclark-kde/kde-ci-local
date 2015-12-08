68a7e6da12322595f2db1304910e90668a45d972
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2015-12-07 17:34:42 -0800

    repo

--- helper/Platform.groovy
+++ helper/Platform.groovy
@@ -109,7 +109,7 @@ class Platform {
 	def GenerateSCM(track) {	
 		this.RepoData.each { path ->
 			this.path = path.key			
-			def protocol = path.find { key, value -> key == 'protocol' }
+			def protocol = path.value.find { key, value -> key == 'protocol' }
 			
 			def address = protocol.find { key, value -> key == 'address' }
 			this.branch = path.branch."${protocol}".find { key, value -> key == track }
