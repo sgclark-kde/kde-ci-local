17a6c04ac57baf0f834eb70394d9340b7f9552d4
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2015-12-07 20:09:05 -0800

    typo

--- helper/Platform.groovy
+++ helper/Platform.groovy
@@ -61,7 +61,7 @@ class Platform {
 			this.path = path.key			
 			this.RepoData = [:] << path.value
 			this.branch = RepoData.branch.value	
-			this.showbrowser = RepoData.showbrowser.value
+			this.showbrowser = RepoData.showwebgit.value
 			this.RepoData.protocol.each { protocolinfo ->
 				this.address = protocolinfo.address.value
 				this.protocol = protocolinfo.key
