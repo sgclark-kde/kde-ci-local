3f687fdce88468d98f35002958b31bd8f1c2c929
Scarlett Clark <sgclark@kde.org>
2016-03-07 19:39:24 -0800

    Correct the url to the actual request.

--- tools/prepare_master_jenkins.py
+++ tools/prepare_master_jenkins.py
@@ -58,7 +58,7 @@ for x in data_file:
   all_plugins = (x['plugins'])
   for plugin in all_plugins:
     data = '<jenkins><install plugin="' + plugin + '@latest" /></jenkins>'
-    url = 'https://build-sandbox.kde.org/pluginManager/installNecessaryPlugins'
+    url = '192.168.1.118/pluginManager/installNecessaryPlugins'
     req = urllib2.Request(url, data, {'Content-Type': 'text/xml'})
     f = urllib2.urlopen(req)
     for y in f:
